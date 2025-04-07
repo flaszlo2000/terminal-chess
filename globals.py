cc89c021d104f28d72f780635466387ad8cba026
flaszlo2000 <f.laszlo2000@gmail.com>
2025-04-07 02:52:26 +0200

    added bigger square height

--- globals.py
+++ globals.py
@@ -1,6 +1,6 @@
 from typing import Final, List
 
-ALLOWED_SQUARE_HEIGHTS: Final[List[int]] = [1, 3, 5] # TODO: react to resize and apply corresponding size if needed
+ALLOWED_SQUARE_HEIGHTS: Final[List[int]] = [1, 3, 5, 7] # TODO: read this from env?
 
 DEFAULT_TABLE: Final[List[str]] = [ # TODO: Add option to change this to unicode pieces!
     "RNBQKBNR",
